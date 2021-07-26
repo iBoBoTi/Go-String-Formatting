bca5892846edec7a12026cbccdd687a26f601f10
Decagon <mac@macdeMacBook-Pro.local>
2021-07-26 23:43:45 +0100

    string formatting assignment

---
+++ strings.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main(){
+	var name string = "Iroegbu Chukwuebuka"
+	var stack string = "Go Stack"
+	var squad string = "SQ008"
+	var year uint16 = 2021
+	fmt.Printf("My name is %s, I am %s developer a member of squad %s of year %d\n",name,stack,squad,year)
+}
